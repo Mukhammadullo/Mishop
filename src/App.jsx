da4385f58691c85e949526ab84c204cf8e0379aa
Muhammadullo <mnastulloev@gmail.com>
2024-02-02 17:34:09 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, ullam?</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
